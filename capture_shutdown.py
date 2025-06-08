28e31bee48bd168b66537ce907a950111bd012ca
mahmud-r-farhan <mdmanna.2020.manna@gmail.com>
2025-06-08 16:48:38 +0600

    capture & shutdown

--- capture_shutdown.py
+++ capture_shutdown.py
@@ -1,1 +1,21 @@
-print("hello world")
+import cv2
+import os
+import time
+from datetime import datetime
+
+folder = "intruder_logs"
+os.makedirs(folder, exist_ok=True)
+
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+
+if ret:
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = os.path.join(folder, f"intruder_{timestamp}.jpg")
+    cv2.imwrite(filename, frame)
+
+cap.release()
+time.sleep(1)
+
+# Shutdown Windows immediately
+os.system("shutdown /s /t 0")
